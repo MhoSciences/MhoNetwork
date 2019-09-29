61e626c9bbc58d3f73525821dc55c49ab4a5c0ef
JosephHaun <riddlemethis565@gmail.com>
2019-09-29 15:55:21 -0700

    Fixing the unfixed stuff

--- NetworkBoard.X/network_functions.c
+++ NetworkBoard.X/network_functions.c
@@ -13,12 +13,18 @@ int _setupNetworkDefaults(char packetHeader[]){
     packetHeader[2] = _getHardwareAddress(0x00); //HIDL: Hardware address low byte
     packetHeader[3] = 0x00; //SIDH: Software Address high byte
     packetHeader[4] = 0x00; //SIDL: Software Address low byte
-    packetHeader[5] = 0x00; //QUEUE: Available packet storage left
-    packetHeader[6] = 0x00; //PRIORITY: Priority byte  
-    packetHeader[7] = 0x00; //MSGH: Message count high byte
-    packetHeader[8] = 0x00; //MSGL: Message count low byte
-    packetHeader[9] = 0x00; //FLG: Flag byte
-    packetHeader[10] = 
+    packetHeader[5] = 0x00; //SID3: 
+    packetHeader[6] = 0x00; //QUEUE: Available packet storage left
+    packetHeader[7] = 0x00; //PRIORITY: Priority byte  
+    packetHeader[8] = 0x00; //MSGH: Message count high byte
+    packetHeader[9] = 0x00; //MSGL: Message count low byte
+    packetHeader[10] = 0x00; //FLG: Flag byte
+    packetHeader[11] = 0x02; //STX byte
+    packetHeader[12] = 0x03; //ETX byte
+    packetHeader[13] = 0x00; //CHKSUM1: First checksum byte
+    packetHeader[14] = 0x00; //CHKSUM2: Second checksum byte
+    packetHeader[15] = 0x04; //EOT byte
+    return
 }
 
 char _getHardwareAddress(char byteNum){
